8edea4c388c033389994280eea225e7a016fc729
Gleboostin-333 <ddgleb@gmail.com>
2025-09-08 20:25:40 +0600

    Created models and added аfields

---
+++ src/main/java/com/library/system/model/Book.java
@@ -0,0 +1,9 @@
+package com.library.system.model;
+
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private String description;
+    private Double price;
+}
---
+++ src/main/java/com/library/system/model/User.java
@@ -0,0 +1,12 @@
+package com.library.system.model;
+
+public class User {
+    private Long id;
+    private String name;
+    private String surname;
+    private String password;
+    private Integer age;
+    private String mail;
+    private String numberOfPhone;
+
+}
